6a826583d2f3b1b4a448c868381d1ef667acc64d
Zahin Afsar <afsarzahin@gmail.com>
2022-06-12 17:18:20 +0600

    Optimised for IOS

--- ios/makerealapp/AppDelegate.mm
+++ ios/makerealapp/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Blinkist", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"makerealapp", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
